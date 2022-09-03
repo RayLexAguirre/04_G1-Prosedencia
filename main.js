ce77f5e2e34a20a9c4a628738722031df4f8c71a
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2022-09-03 17:55:38 -0500

    Moved

---
+++ main.js
@@ -0,0 +1,17 @@
+
+let x = 5 + 2 * 3;
+
+console.log(x);
+
+//se respetan las leyes de los signos
+let z = (5 + 2) * 3;
+
+console.log(z);
+
+let w = 5 + (2 * 4) + (9 / 2 + 7);
+
+console.log(w);
+
+let y = (5 * 4) / 2
+
+ console.log(y);
